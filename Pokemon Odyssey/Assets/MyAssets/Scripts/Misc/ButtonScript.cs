2557cbe0b38d0174d85664cea424cdbc005081f2
MatthewRadeff <matt_radeff@hotmail.com>
2017-12-28 23:55:20 -0500

    main menu
    main menu almost done. Its how I wanted it to look, so now it just needs functionality to move to different scenes. Later on in the project there will be more polish.

---
+++ Pokemon Odyssey/Assets/MyAssets/Scripts/Misc/ButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    // selects which scene to go to
+	public void ChangeScene(string m_sceneName)
+    {
+        SceneManager.LoadScene(m_sceneName);
+
+    }
+
+    // will have to add in an "async" later for loading screens... 
+
+
+
+    // quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
